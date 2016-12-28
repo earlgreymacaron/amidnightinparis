bd12cf8d6783b9c91f13f7dbbe5689c003088d4b
earlgreymacaron <yeaseul.parky@gmail.com>
2016-12-28 16:52:19 +0900

    first commit

---
+++ app/src/main/java/com/cs496/firstproject5/HotelAdapter.java
@@ -0,0 +1,8 @@
+package com.cs496.firstproject5;
+
+/**
+ * Created by q on 2016-12-27.
+ */
+
+public class HotelAdapter {
+}
---
+++ app/src/main/java/com/cs496/firstproject5/PopDetails.java
@@ -0,0 +1,7 @@
+package com.cs496.firstproject5;
+
+/**
+ * Created by q on 2016-12-28.
+ */
+public class PopDetails {
+}
